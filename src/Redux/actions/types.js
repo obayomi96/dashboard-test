7c0baf7602d437faf208b635691002be01d92fc4
martins obayomi <obayomimartins96@gmail.com>
2020-04-19 10:25:11 +0100

    Setup redux boilerplate

---
+++ src/Redux/actions/types.js
@@ -0,0 +1,6 @@
+export const GET_ORDERS = 'GET_ORDERS';
+export const GET_ORDER_ITEMS = 'GET_ORDER_ITEM';
+export const GET_BUYER = 'GET_BUYER';
+export const GET_PRODUCTS = 'GET_PRODUCTS';
+export const GET_WEEKLY = 'GET_WEEKLY';
+export const GET_WEEKLY_ITEMS = 'GET_WEEKLY_ITEM';
